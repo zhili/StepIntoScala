9abd8909064504f04205834ca97da7fb50e81b50
zhili <huzhili@gmail.com>
2010-05-02 00:09:22 +0800

    rm some unused files

---
+++ git-ignore.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# git rm --cached   target/scala_2.7.7/analysis/applications
+# git rm --cached   target/scala_2.7.7/analysis/dependencies
+# git rm --cached   target/scala_2.7.7/analysis/external
+# git rm --cached   target/scala_2.7.7/analysis/generated_files
+# git rm --cached   target/scala_2.7.7/analysis/hashes
+# git rm --cached   target/scala_2.7.7/analysis/tests
+# git rm --cached   target/scala_2.7.7/test-analysis/dependencies
+# git rm --cached   target/scala_2.7.7/test-analysis/external
+# git rm --cached   target/scala_2.7.7/test-analysis/generated_files
+# git rm --cached   target/scala_2.7.7/test-analysis/hashes
+# git rm --cached   target/scala_2.7.7/test-analysis/tests
+git rm --cached    target/scala_2.7.7/classes/main/scala/Rational.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec$$anonfun$1$$anonfun$apply$1.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec$$anonfun$1.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec$$anonfun$2$$anonfun$apply$2.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec$$anonfun$2.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec$$anonfun$3$$anonfun$apply$3.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec$$anonfun$3.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec$$anonfun$4$$anonfun$apply$4.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec$$anonfun$4.class
+git rm --cached    target/scala_2.7.7/test-classes/test/scala/RationalSpec.class
+
